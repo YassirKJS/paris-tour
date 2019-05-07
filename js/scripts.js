cb942eb864896c2ab18fbc83743a40913c281e88
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-07 16:25:39 +0200

    Typed js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    const typed = new Typed(".typed", {
+        strings: ["Where the magic happens. ^1000"],
+        typeSpeed: 48,
+        loop: false,
+        startDelay: 750,
+        showCursor: false
+    });
+});
