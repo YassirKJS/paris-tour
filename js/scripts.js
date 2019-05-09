1fc1975790d7efd4141c95ccb9b41d8dbb8cf927
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-09 17:49:31 +0200

    section-about photos composition

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    const typed = new Typed(".typed", {
-        strings: ["Where the magic happens. ^1000"],
-        typeSpeed: 48,
-        loop: false,
-        startDelay: 750,
-        showCursor: false
-    });
-});
+const src = document.getElementById("logo");
+const changeOnhover = e => {
+    e.target.setAttribute('src', '../img/output.gif')
+};
+const changeOnUnhover = e => {
+    e.target.setAttribute('src', '../img/logo-white-3.png')
+};
+src.addEventListener("mouseover", changeOnhover);
+src.addEventListener("mouseout", changeOnUnhover);
