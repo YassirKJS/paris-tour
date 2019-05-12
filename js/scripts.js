47fd503bbe1885aa6b04826df6a2666678290c0d
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-12 05:15:19 +0200

    footer + new logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 const src = document.getElementById("logo");
+const src2 = document.getElementById("footer-logo");
+
 const changeOnhover = e => {
     e.target.setAttribute('src', '../img/output.gif')
 };
 const changeOnUnhover = e => {
     e.target.setAttribute('src', '../img/logo-white-3.png')
 };
+
+const changeOnhover2 = e => {
+    e.target.setAttribute('src', '../img/paritours.gif')
+};
+const changeOnUnhover2 = e => {
+    e.target.setAttribute('src', '../img/paritours-pink-logo.png')
+};
+
 src.addEventListener("mouseover", changeOnhover);
 src.addEventListener("mouseout", changeOnUnhover);
+
+src2.addEventListener("mouseover", changeOnhover2);
+src2.addEventListener("mouseout", changeOnUnhover2);
